1b62a65ddccd740df9ea917eadad23a02aa30025
lkssmnt <hello@lsiemoneit.de>
2026-05-07 12:12:12 +0200

    added list of projects and texts to homepage

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,5 +1,23 @@
 <?php snippet("head") ?>
 
-<h1>This is the homepage</h1>
+<h1><?= $site->title() ?></h1>
+
+<h2>Projects</h2>
+<ul>
+  <?php foreach(page("projects")->children() as $projectpage ): ?>
+    <li>
+      <a href="<?= $projectpage->url() ?>"><?= $projectpage->title() ?></a>
+    </li>
+  <?php endforeach ?>
+</ul>
+
+<h2>Texts</h2>
+<ul>
+  <?php foreach(page("texts")->children() as $textpage ): ?>
+    <li>
+      <a href="<?= $textpage->url() ?>"><?= $textpage->title() ?></a>
+    </li>
+  <?php endforeach ?>
+</ul>
 
 <?php snippet("foot") ?>
